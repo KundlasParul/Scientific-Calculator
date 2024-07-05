31ae5a74e9bd3da91b01ab7e8b0165aae7d091bf
Parul Kundlas <152483468+KundlasParul@users.noreply.github.com>
2024-07-05 20:08:11 +0000

    Added a try and catch function

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
-                  
+document.addEventListener("DOMContentLoaded", function () {
+
     const display = document.getElementById('calc-display');
     const buttons = document.getElementsByClassName('btn');
     console.log(buttons);
     console.log(display);
-    
+
     let currentValue = "";
 
-    function evaluateResult(){
-        
+    function evaluateResult() {
+
         console.log('CurrentValue:', currentValue);
 
         let convertedValue = currentValue.replace("×", "*");
@@ -29,24 +29,35 @@ document.addEventListener("DOMContentLoaded", function(){
         display.value = currentValue;
     }
 
-    for(let i = 0; i < buttons.length ; i++){
+    for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
-        button.addEventListener('click', function(){
-           const value = button.innerText;
+        button.addEventListener('click', function () {
+            const value = button.innerText;
 
-           if(value == "AC"){
-                currentValue = "";
+            try {
+
+                if (value == "AC") {
+                    currentValue = "";
+                    display.value = currentValue;
+                }
+                else if (value == "=") {
+
+                    evaluateResult();
+                }
+                else {
+                    currentValue += value;
+                    console.log('CurrentValue:', currentValue);
+                    display.value = currentValue;
+                }
+
+            } catch(error){
+                     
+                console.error(error);
+                currentValue = "ERROR";
                 display.value = currentValue;
-           }
-           else if(value == "="){
-                
-                 evaluateResult();
-           }
-           else{
-            currentValue += value;
-            console.log('CurrentValue:', currentValue);
-            display.value = currentValue;
-           }
+                )
+            }
+           
            
         })
     }
