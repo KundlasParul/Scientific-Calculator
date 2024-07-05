7b237e80e4b5b2dbc75647025d1cc6ce4b0ad0f0
Parul Kundlas <152483468+KundlasParul@users.noreply.github.com>
2024-07-05 18:57:28 +0000

    Seperated the script file

---
+++ src/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function(){
+                  
+    const display = document.getElementById('calc-display');
+    const buttons = document.getElementsByClassName('btn');
+    console.log('buttons');
+    console.log('display');
+    
+    let currentValue = "";
+
+    function evaluateResult(){
+        
+        console.log('CurrentValue:', currentValue);
+
+        const convertedValue = currentValue.replace("×", "*");
+        convertedValue = currentValue.replace("÷", "/");
+        convertedValue = currentValue.replace("%", "*0.01");
+
+        console.log('currentValue:', convertedValue);
+        const result = eval(convertedValue);
+        currentValue = result.tostring();
+        display.value = currentValue;
+    }
+
+    for(let i = 0; i < buttons.length ; i++){
+        const button = buttons[i];
+        button.addEventListener('click', function(){
+           const value = button.InnerText;
+
+           if(value == 'AC'){
+                currentValue = "";
+                display.value = currentValue;
+           }
+           else if(value == "="){
+                
+                 evaluateResult();
+           }
+           else{
+            currentValue += value;
+            console.log('CurrentValue:', currentValue);
+            display.value = currentValue;
+           }
+           
+        })
+    }
+})
